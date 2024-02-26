2462846f2a64c5dadab93e3d605e601c09e5342d
Willian Mikhael da Cunha <willian_mikhael@outlook.com>
2024-02-26 09:11:43 -0300

    resumeadjustments

--- src/components/Resumepage.vue
+++ src/components/Resumepage.vue
@@ -1,114 +1,120 @@
 <template>
-    <div class="mb-32">
-        <div class="my-10 p-6 max-w-8xl rounded-3xl bg-fball">
-            <section class="grid grid-cols-2 mb-12">
-                <div class="column-span-1 m-8 p-16">
-                    <p class="text-4xl font-Merriweather italic">Willian Mikhael</p>
-                </div>
-                <div class="column-span-1 m-8 p-12 flex justify-end">
-                    <ul class="flex flex-col mr-16">
-                        <li>willianmikhael.com.br</li>
-                        <li>willian.mikhael.lol@gmail.com</li>
-                        <li>linkedin.com/in/willianmikhael</li>
-                    </ul>
-                </div>
-            </section>
-            <section class="grid grid-cols-3 gap-4 mb-52 mt-8">
-                <div class="col-span-1  p-4 ml-12 pl-11 font-Roboto text-xl ">
-                    EXPERIENCE
-                </div>
-                <div class="col-span-2 p-4">
-                    <ul>
-                        <li class="pb-2">
-                            <h1 class="text-xl font-bold">Brazilian Army • Soldier</h1>
-                            <p class="text-gray-400">2019 | Joinville, SC</p>
-                            <p class="text-lg">Military Training</p>
-                        </li>
+    <div class="mb-32 max-w-6xl  ">
+        <div class="flex justify-center ml-24 ">
+            <div class="my-10 p-6 rounded-3xl bg-fball">
+                <section class="grid grid-cols-2 mb-12">
+                    <div class="column-span-1 m-8 p-16">
+                        <p class="text-4xl font-Merriweather italic ml-4">Willian Mikhael</p>
+                    </div>
+                    <div class="column-span-1 m-8 p-12 flex justify-end">
+                        <ul class="flex flex-col mr-16">
+                            <li>willianmikhael.com.br</li>
+                            <li>willian.mikhael.lol@gmail.com</li>
+                            <li>linkedin.com/in/willianmikhael</li>
+                        </ul>
+                    </div>
+                </section>
+                <section class="grid grid-cols-3 gap-4 mb-52 mt-8">
+                    <div class="col-span-1  p-4 ml-16 pl-11 font-Roboto text-xl ">
+                        EXPERIENCE
+                    </div>
+                    <div class="col-span-2 p-4">
+                        <ul>
+                            <li class="pb-2">
+                                <h1 class="text-xl font-bold">Brazilian Army • Soldier</h1>
+                                <p class="text-gray-400">2019 | Joinville, SC</p>
+                                <p class="text-lg">Military Training</p>
+                            </li>
 
-                    </ul>
-
-
-                </div>
-                <!-- ---------------------------------------------------------------------------------------------------------- -->
-                <div class="col-span-1  p-4">
-
-                </div>
-                <div class="col-span-2  p-4">
-                    <ul>
-                        <li class="pb-2">
-                            <h1 class="text-xl font-bold"></h1>
-                            <p class="text-gray-400"></p>
-                            <p class="text-lg">I have worked in several companies, but <br> I'm currently looking for my
-                                first job in software engineering</p>
-                        </li>
-
-                    </ul>
+                        </ul>
 
 
-                </div>
+                    </div>
+                    <!-- ---------------------------------------------------------------------------------------------------------- -->
+                    <div class="col-span-1  p-4">
 
-            </section>
-            <section class="grid grid-cols-3 gap-4">
-                <div class="col-span-1  p-4 ml-12 pl-11 font-Roboto text-xl ">
-                    EDUCATION
-                </div>
-                <div class="col-span-2 font-Roboto p-4">
-                    <ul>
-                        <li class="pb-2">
-                            <h1 class="text-xl font-bold">Leonardo Da Vinci University</h1>
-                            <p class="text-gray-400">Aug 2020 - Dez 2022</p>
-                            <p class="text-lg">Forensic Investigation and Criminal Expertise</p>
-                        </li>
-                        <li class="pb-2">
-                            <h1 class="text-xl font-bold">Catholic University of Santa Catarina</h1>
-                            <p class="text-gray-400">Aug 2022 - Current</p>
-                            <p class="text-lg">Software Engieering</p>
-                        </li>
-                        <li class="pb-2">
-                            <h1 class="text-xl font-bold">TopWay English School</h1>
-                            <p class="text-gray-400">Jan 2023 - Current</p>
-                            <p class="text-lg">Intesive English Course</p>
-                        </li>
-                    </ul>
-                    <p></p>
-                </div>
-            </section>
+                    </div>
+                    <div class="col-span-2  p-4">
+                        <ul>
+                            <li class="pb-2">
+                                <h1 class="text-xl font-bold"></h1>
+                                <p class="text-gray-400"></p>
+                                <p class="text-lg">I have worked in several companies, but <br> I'm currently looking for my
+                                    first job in software engineering</p>
+                            </li>
 
-            <section class="grid grid-cols-3 gap-4 mt-8">
-                <div class="col-span-1  p-4 ml-12 pl-11 font-Roboto text-xl ">
-                    SKILLS
-                </div>
-                <div class="col-span-2  p-4">
-                    <ul>
-                        <li class="pb-2">
-                            <h1 class="text-xl font-bold">  <>  </h1>
-                            <p class="text-lg">HTML, CSS, BootStrap, TailWind, <br>
-                                JavaScript, C, Python, Java, SQL, Git, <br>
-                                VMs, Linux, Windows, Office.</p>
-                        </li>
+                        </ul>
 
-                    </ul>
-                </div>
-            </section>
+
+                    </div>
+
+                </section>
+                <section class="grid grid-cols-3 gap-4">
+                    <div class="col-span-1  p-4 ml-16 pl-11 font-Roboto text-xl ">
+                        EDUCATION
+                    </div>
+                    <div class="col-span-2 font-Roboto p-4">
+                        <ul>
+                            <li class="pb-2">
+                                <h1 class="text-xl font-bold">Leonardo Da Vinci University</h1>
+                                <p class="text-gray-400">Aug 2020 - Dez 2022</p>
+                                <p class="text-lg">Forensic Investigation and Criminal Expertise</p>
+                            </li>
+                            <li class="pb-2">
+                                <h1 class="text-xl font-bold">Catholic University of Santa Catarina</h1>
+                                <p class="text-gray-400">Aug 2022 - Current</p>
+                                <p class="text-lg">Software Engieering</p>
+                            </li>
+                            <li class="pb-2">
+                                <h1 class="text-xl font-bold">TopWay English School</h1>
+                                <p class="text-gray-400">Jan 2023 - Current</p>
+                                <p class="text-lg">Intesive English Course</p>
+                            </li>
+                        </ul>
+                        <p></p>
+                    </div>
+                </section>
+
+                <section class="grid grid-cols-3 gap-4 my-8">
+                    <div class="col-span-1  p-4 ml-16 pl-11 font-Roboto text-xl ">
+                        SKILLS
+                    </div>
+                    <div class="col-span-2  p-4">
+                        <ul>
+                            <li class="pb-2">
+                                <h1 class="text-xl font-bold">
+                                    <>
+                                </h1>
+                                <p class="text-lg">HTML, CSS, BootStrap, TailWind, <br>
+                                    JavaScript, C, Python, Java, SQL, Git, <br>
+                                    VMs, Linux, Windows, Office.</p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </section>
+            </div>
         </div>
-        <section class="flex justify-end">
-
-            <section class="flex justify-end">
-
-                <a href="https://docs.google.com/document/d/1M0Oh7XiEm2vMDT_uBzw-hU8F4OIi-Y1NFDdJ4c-b1nc/edit?usp=sharing" target="_blank">
-                    <button class=" hover:bg-gray-300 text-gray-400 font-bold p-3 px-4 m-2 rounded-full">
-                        Dowload CV Pt
-                    </button>
-                </a>
-
-                <a href="https://docs.google.com/document/d/1sDRDgqjC7UwR_L3lo5ybjJF1RjPxZxjy_1EJ2w77zYA/edit?usp=sharing" target="_blank">
-                    <button class=" hover:bg-gray-300 text-gray-400 font-bold p-3 px-4 m-2 rounded-full">
-                        Dowload CV Us
-                    </button>
-                </a>
+        <section class="flex justify-end mr-6">
 
 
-            </section>
+
+            <a href="https://docs.google.com/document/d/1M0Oh7XiEm2vMDT_uBzw-hU8F4OIi-Y1NFDdJ4c-b1nc/edit?usp=sharing"
+                target="_blank">
+                <button class=" hover:bg-gray-200 text-gray-400 font-bold p-3 px-4 m-2 rounded-full">
+                    Dowload CV Pt
+                </button>
+            </a>
+
+            <a href="https://docs.google.com/document/d/1sDRDgqjC7UwR_L3lo5ybjJF1RjPxZxjy_1EJ2w77zYA/edit?usp=sharing"
+                target="_blank">
+                <button class=" hover:bg-gray-200 text-gray-400 font-bold p-3 px-4 m-2 rounded-full mr-16">
+                    Dowload CV Us
+                </button>
+            </a>
+
+
+
 
         </section>
 
